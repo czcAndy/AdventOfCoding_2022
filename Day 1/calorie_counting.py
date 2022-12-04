d7ae0add374eb07818f89515dc5ffc1da2a3d2e1
Andrei Cazaceanu <andrei.cazaceanu@gmail.com>
2022-12-04 21:57:58 +0100

    Day 1 - refactored

--- Day 1/calorie_counting.py
+++ Day 1/calorie_counting.py
@@ -7,11 +7,10 @@ with requests.Session() as s:
     s.cookies.set('session', os.environ['AOC_2022_COOKIE'])
     resp = s.get('https://adventofcode.com/2022/day/1/input')
 
-
 grouped_res = groupby(resp.iter_lines(), lambda x: x != b'')
-grouped_res = [(map(int, list(g))) for k, g in grouped_res if k]
+grouped_res = [list(map(int, list(g))) for k, g in grouped_res if k]
 
-#1
+# 1
 print(max(map(sum, grouped_res)))
 
 # 2
